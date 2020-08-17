1f2dd3d51f457674aaff5590540d498e3cff104d
admyavuzs <sayilgan.yavuz@gmail.com>
2020-08-18 01:41:27 +0300

    app resources

---
+++ hello-world.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "fmt"
+    "html"
+    "log"
+    "net/http"
+    "time"
+    "github.com/prometheus/client_golang/prometheus"
+    "github.com/prometheus/client_golang/prometheus/promauto"
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+)
+func recordMetrics() {
+        go func() {
+                for {
+                        opsProcessed.Inc()
+                        time.Sleep(2 * time.Second)
+                }
+        }()
+}
+
+var (
+        opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+                Name: "myapp_processed_ops_total",
+                Help: "The total number of processed events",
+        })
+)
+
+func main() {
+    
+    recordMetrics()
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    })
+
+    http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+        fmt.Fprintf(w, "Hi poweredit from @yavuzs")
+    })
+
+    http.Handle("/metrics", promhttp.Handler())
+    
+    log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
